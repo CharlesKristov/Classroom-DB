7c01fc142edb9bae82d2c9bd4b3ff3dece0ecef2
oiko78 <oliver.chico@binus.ac.id>
2021-12-25 11:20:06 +0700

    feat: add material model

---
+++ model/Material.php
@@ -0,0 +1,19 @@
+<?php 
+  class Material {
+    private $db;
+
+    public function __construct(PDO $db) {
+      $this->db = $db;
+    }
+
+    public function getMaterial($material_id) {
+      try {
+        $statement = $this->db->prepare("SELECT * FROM `material` WHERE `id` = {$material_id}`");
+      } catch(PDOException $e) {
+        echo $e;
+        return null;
+      }
+      return $statement->fetchAll(PDO::FETCH_ASSOC);
+    }
+  }
+?>

f89da587518bbc272d4654920949294e05e8292a
SusuMantan <suzumantan@gmail.com>
2021-12-29 14:41:27 +0000

    feat: add pretty-print-json CDN
    Co-authored-by: Charles Christopher <CharlesKristov@users.noreply.github.com>
    Co-authored-by: Oliver Chico <oliverchiko2002@gmail.com>

--- index.php
+++ index.php
@@ -24,6 +24,7 @@ $page = isset($_SESSION['page']) ? $_SESSION['page'] : 'login';
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
+  <link rel=stylesheet href=https://cdn.jsdelivr.net/npm/pretty-print-json@1.1/dist/pretty-print-json.css>
   <title>DB-WebApp</title>
 </head>
 
@@ -43,6 +44,7 @@ $page = isset($_SESSION['page']) ? $_SESSION['page'] : 'login';
 
   <!-- Option 1: Bootstrap Bundle with Popper -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  <script src=https://cdn.jsdelivr.net/npm/pretty-print-json@1.1/dist/pretty-print-json.min.js></script>
 </body>
 
 </html>

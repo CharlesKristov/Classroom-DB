243454b70363501ebf07a19889dc15df1a0957bf
Made Sukma <suzumantan@gmail.com>
2022-01-02 23:23:32 +0700

    fix: change website title

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ $page = isset($_SESSION['page']) ? $_SESSION['page'] : 'login';
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
   <link rel=stylesheet href=https://cdn.jsdelivr.net/npm/pretty-print-json@1.1/dist/pretty-print-json.css>
-  <title>DB-WebApp</title>
+  <title>Classroom DB</title>
 </head>
 
 <body>
